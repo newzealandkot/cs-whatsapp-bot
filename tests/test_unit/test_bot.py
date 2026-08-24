db5efbe44994147ddc361d8592e7b2d08236ea46
newzealand <newzealandkot@gmail.com>
2026-08-24 23:12:35 +0500

    add bot fixture

--- tests/test_unit/test_bot.py
+++ tests/test_unit/test_bot.py
@@ -2,29 +2,30 @@ import pytest
 from src.cs_whatsapp_bot.bot import Bot, BotInactiveError, MESSAGE
 
 
-def test_can_start_bot():
-    bot = Bot()
+@pytest.fixture
+def bot():
+    return Bot()
+
+
+def test_can_start_bot(bot):
     bot.is_active = False
     bot.start()
     assert bot.is_active
 
 
-def test_can_stop_bot():
-    bot = Bot()
+def test_can_stop_bot(bot):
     bot.is_active = True
     bot.stop()
     assert not bot.is_active
 
 
-def test_bot_can_return_message():
-    bot = Bot()
+def test_bot_can_return_message(bot):
     bot.start()
     message = bot.message()
     assert message == MESSAGE
 
 
-def test_can_message_only_active_bot():
-    bot = Bot()
+def test_can_message_only_active_bot(bot):
     bot.is_active = False
     exc_message = "Bot is inactive, call 'start' method to activate bot"
     with pytest.raises(BotInactiveError, match=exc_message):
