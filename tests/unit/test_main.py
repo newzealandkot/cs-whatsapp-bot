e6e7ed74b91addf95f19b0f85e4b860ea5213f43
newzealand <newzealandkot@gmail.com>
2026-08-24 22:30:02 +0500

    deleted unit folder

--- tests/unit/test_main.py
+++
@@ -1,6 +0,0 @@
-def test_whatsapp_cloud_api_is_installed() -> None:
-    try:
-        import whatsapp_cloud_api
-    except ModuleNotFoundError:
-        assert False
-    assert whatsapp_cloud_api is not None
